cf146209638a7871466c97c07375333b9d973f9a
Nagico <yjr888@vip.qq.com>
2023-04-24 22:55:36 +0800

    feat: 添加cors

--- src/main/kotlin/cn/nagico/teamup/backend/server/StompWebSocketChatServerInitializer.kt
+++ src/main/kotlin/cn/nagico/teamup/backend/server/StompWebSocketChatServerInitializer.kt
@@ -2,6 +2,7 @@ package cn.nagico.teamup.backend.server
 
 
 import cn.nagico.teamup.backend.enums.StompVersion
+import cn.nagico.teamup.backend.handler.CorsHandler
 import cn.nagico.teamup.backend.util.StompWebSocketProtocolCodec
 import io.netty.channel.ChannelInitializer
 import io.netty.channel.socket.SocketChannel
@@ -19,10 +20,14 @@ class StompWebSocketChatServerInitializer : ChannelInitializer<SocketChannel>() 
     @Autowired
     private lateinit var stompWebSocketProtocolCodec: StompWebSocketProtocolCodec
 
+    @Autowired
+    private lateinit var corsHandler: CorsHandler
+
     override fun initChannel(channel: SocketChannel) {
         channel.pipeline()
             .addLast(HttpServerCodec())
             .addLast(HttpObjectAggregator(65536))
+            .addLast(corsHandler)
             .addLast(WebSocketServerProtocolHandler(chatPath, StompVersion.SUB_PROTOCOLS))
             .addLast(stompWebSocketProtocolCodec)
     }
